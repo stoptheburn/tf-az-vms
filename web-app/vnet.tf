3b1ef225817b2e6f3618355fc36ff5d9520c69c9
Rakshit <rakshit@macbookair.lan>
2025-03-10 15:07:51 -0500

    Added ssh nsg to vnet.tf

--- web-app/vnet.tf
+++ web-app/vnet.tf
@@ -95,6 +95,18 @@ resource "azurerm_network_security_group" "app_nsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 320
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = {
     environment = "Production"
   }
