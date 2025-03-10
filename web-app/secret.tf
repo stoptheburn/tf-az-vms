15be38785507446ef16a76434e543b805931c03b
Rakshit <rakshit@macbookair.lan>
2025-03-10 15:15:50 -0500

    Updated secret.tf

--- web-app/secret.tf
+++ web-app/secret.tf
@@ -5,7 +5,7 @@ resource "azurerm_key_vault" "apprstfkeyvalut" {
   resource_group_name         = azurerm_resource_group.appgrp.name
   enabled_for_disk_encryption = true
   #tenant_id                   = data.azurerm_client_config.current.tenant_id
-  tenant_id                   = "3f3fd244-2e4a-41e7-9d36-84d599d0b6ed"
+  tenant_id                   = var.tenant_id # created a new security-rs-grp
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
